a490051800fb60f638018464e5137696d8d9a29a
jtreinke <jenniferreinke@x-10-104-41-199.uofm-secure.wireless.umn.edu>
2015-09-24 12:40:39 -0500

    Add homepage to folder

---
+++ scrollcopy/js/script.js
@@ -0,0 +1,56 @@
+$(document).scroll(function(){
+
+	var y = $(document).scrollTop();
+	$("#update").html(y);
+
+	if (y > 100) {
+		$("#one").fadeIn();
+	}
+	else {
+		$("#one").fadeOut();
+	}
+	if (y > 250) {
+		$("#two").fadeIn();
+	}
+	else {
+		$("#two").fadeOut();
+	}
+	if (y > 400) {
+		$("#three").fadeIn();
+	}
+	else {
+		$("#three").fadeOut();
+	}
+	if (y > 600) {
+		$("#four").fadeIn();
+	}
+	else {
+		$("#four").fadeOut();
+	}
+	if (y > 800) {
+		$("#five").fadeIn();
+	}
+	else {
+		$("#five").fadeOut();
+	}
+	if (y > 950) {
+		$("#six").fadeIn();
+	}
+	else {
+		$("#six").fadeOut();
+	}
+	
+	if (y > 1200) {
+		$("#container").css({"position": "relative"});
+		$("#container").css({"top": "0px"});
+	}
+	
+	else {
+	$("#container").css({"position": "fixed"});
+	$("#container").css({"top": "0px"});
+	}
+
+});
+
+
+
